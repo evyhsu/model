993bd174fcec3a2b4714e3e8a8899437e18e41b5
yenchu <yenchu@yenchu.(none)>
2013-09-24 11:45:26 +0800

    all file

---
+++ yen-ss-handover.cc
@@ -0,0 +1,143 @@
+/*
+This file is writen by yenchu hsu
+here is the email you can contect: yenchsu@gmail.com
+*/
+
+#include "yen-ss-handover.h"
+
+NS_LOG_COMPONENT_DEFINE ("YenSSHandover");
+
+namespace ns3 {
+NS_OBJECT_ENSURE_REGISTERED (YenSSHandover);
+
+TypeId YenSSHandover::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::YenSSHandover")
+    .SetParent<Header> ();
+  return tid;
+}
+
+YenSSHandover::YenSSHandover (void)
+{
+}
+
+YenSSHandover::~YenSSHandover (void)
+{
+}
+
+uint8_t 
+YenSSHandover::getNbNeighbor (void) const
+{
+	return m_nbentry;
+}
+
+bool
+YenSSHandover::isDetected(void) const
+{
+	return m_detected_;
+}
+
+void
+YenSSHandover::Start (void)
+{
+/******inoder to let ss-net-device.cc compiler successful, add it ******/
+}
+
+void
+YenSSHandover::Stop (void)
+{
+/******inoder to let ss-net-device.cc compiler successful, add it ******/
+}
+
+bool
+YenSSHandover::DoSend (Ptr<Packet> packet,
+                       const Mac48Address &source,
+                       const Mac48Address &dest,
+                       uint16_t protocolNumber)
+{
+  return 0;
+/******inoder to let ss-net-device.cc compiler successful, add it ******/
+}
+
+void
+YenSSHandover::DoReceive (Ptr<Packet> packet)
+{
+/******inoder to let ss-net-device.cc compiler successful, add it ******/
+}
+
+bool
+YenSSHandover::Enqueue (Ptr<Packet> packet,
+                        const MacHeaderType &hdrType,
+                        Ptr<WimaxConnection> connection)
+{
+  return 0;
+/******inoder to let ss-net-device.cc compiler successful, add it ******/
+}
+
+void
+YenSSHandover::InitYenSSHandover (void)
+{
+	//m_n_new_bs_index = 0;
+	//m_report_metric = 0*2;
+	m_size = 0;
+	m_tmp = 0;
+	m_tmpB = 0;
+}
+
+uint8_t
+YenSSHandover::getMOB_MSHO_REQ_size (void)
+{	
+
+    std::cout << "yensshandover" << std::endl;
+	
+	if (m_n_new_bs_index !=0) 
+    	m_size++;
+
+	if (m_report_metric & 0x1) m_tmp++;
+  	if (m_report_metric & 0x2) m_tmp++;
+  	if (m_report_metric & 0x4) m_tmp++;
+
+	/*for (uint8_t i = 0 ; i < m_n_new_bs_index ; i++) {
+	    m_tmpB += 20 + 8*m_tmp;    
+      if (bs_index[i].arrival_time_diff_ind & 0x1)//000000000000000000000000000000000
+      	m_tmpB += 4;
+  }*/
+
+  	m_size += m_tmp/8;
+  	if ((m_tmp%8)!=0)
+    	m_size ++;  //includes padding
+  
+  	return m_size;
+}
+
+void 
+YenSSHandover::send_msho_req (Ptr<Packet> packet,
+							  const MacHeaderType &hdrType,
+							  )
+{
+    MshoReq mshoreq;//define at mac-messages.cc, replace Mac80216MobMshoReqFrame *m_req_frame;
+	NeighborDB *m_nbrdb
+
+	//MacHeaderType wimaxHdr; 
+
+ 
+
+	m_nbPref = 0;
+  	for (uint8_t i = 0 ; i < m_nbrdb->getNbNeighbor() ; i++) {
+   	  m_entry = m_nbrdb->getNeighbors(i);
+		if (m_entry->isDetected(){
+		  m_nbPref++;
+ 	 }
+ 	}
+		if (m_nbPref=0)
+		  return; //no other BS found
+//create packet for request
+
+   packet->sizeof (MshoReq); //replace ns2 p = getPacket ();
+
+
+
+
+
+
+}// namespace ns3
---
+++ yen-ss-handover.h
@@ -0,0 +1,70 @@
+
+/*
+This file is writen by yenchu hsu
+here is the email you can contect: yenchsu@gmail.com
+*/
+
+#ifndef YEN_SS_HANDOVER_H
+#define YEN_SS_HANDOVER_H
+
+#include "wimax-net-device.h"
+#include "wimax-mac-queue.h"
+#include "ns3/event-id.h"
+#include "ns3/uinteger.h"
+#include "ns3/packet.h"
+#include "ss-net-device.h"
+namespace ns3 {
+
+//class Packet
+/*class HdrCmn
+class HdrMac80216
+class Mac80216MobMshoReqFrame
+class NeighborDB
+class WimaxNeighborEntry*/
+
+class YenSSHandover : public WimaxNetDevice
+{
+public:
+  static TypeId GetTypeId (void);//each class must implement a static public member function called GetTypeId (void)
+  YenSSHandover (void);
+  ~YenSSHandover (void);
+
+  //void send_msho_req (void);
+  void InitYenSSHandover(void);
+  void send_msho_req (Ptr<Packet> packet);
+
+  uint8_t getMOB_MSHO_REQ_size(void);
+  uint8_t getNbNeighbor (void) const;
+  
+  bool isDetected(void) const;//for inheritance from wimaxnetdevice
+  void Start (void);
+  void Stop (void);
+  bool Enqueue (Ptr<Packet> packet, const MacHeaderType &hdrType, Ptr<WimaxConnection> connection);
+
+
+};
+private:
+    bool DoSend (Ptr<Packet> packet, const Mac48Address &source, const Mac48Address &dest, uint16_t protocolNumber);
+    void DoReceive (Ptr<Packet> packet);
+	
+	//parameter from getMOB_MSHO_REQ_size
+	uint8_t m_size;
+	uint8_t m_tmp;
+	uint8_t m_tmpB;
+
+	//parameter from send_msho_req ()
+	double rssi;
+	uint8_t m_nbentry;
+	uint8_t m_nbPref;
+    uint8_t m_detected_;
+	// set pointer for send_msho_req (void)
+	
+	//ptr<WimaxNeighborEntry> m_entry;
+};//class YenSSHandover
+
+} // namespace ns3
+
+#endif /* YEN_SS_HANDOVER_H */
+
+
+
